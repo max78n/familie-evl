bbb7edebc4178ec6140d020ebd7487687c3fb200
Markus Eckerstorfer <markuseckerstorfer@Markuss-MacBook-Pro.local>
2026-04-19 20:48:31 +0200

    Bruker miljøvariabler for Supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://lkxfmouyutctehbydwsj.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_MjLQIJwR1tQq5gXEhblVVA_0tIS05Zw'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
